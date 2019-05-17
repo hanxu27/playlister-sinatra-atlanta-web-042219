6eede5caf041c0baec2f1b3afc9f599c927eafc5
Han Xu <hanxu27@gmail.com>
2019-05-16 20:25:56 -0400

    functionalty done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongController < Sinatra::Base
+
     configure do
         set :views, 'app/views/songs'
     end
@@ -16,10 +17,11 @@ class SongController < Sinatra::Base
         @artist = Artist.find_or_create_by(name: params[:artist])
         @song = Song.new(name: params[:name], artist: @artist)
 
-        if @song.save!
+        if @song.save
             params[:genres].each do |g_id|
                 SongGenre.create(song_id: @song.id, genre_id: g_id)
             end
+            flash[:message] = "Successfully created song."
             redirect "/songs/#{Slug.slugify(@song.name)}"
         else 
             @errors = @song.errors.full_messages 
@@ -34,5 +36,21 @@ class SongController < Sinatra::Base
         erb :show
     end
 
+    get '/songs/:slug/edit' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :edit
+    end
 
+    patch '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        @artist = Artist.find_or_create_by(name: params[:Song][:artist])
+        @song.update(name: params[:Song][:name], artist: @artist)
+        @song.song_genres.each { |sg| sg.destroy }
+        @song.reload
+        params[:genres].each do |g_id|
+            SongGenre.create(song_id: @song.id, genre_id: g_id)
+        end
+
+        redirect "/songs/#{Slug.slugify(@song.name)}"
+    end
 end
